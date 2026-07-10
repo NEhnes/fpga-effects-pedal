d6be66ee6f67dcb1afc3381caec7f999dbb66b18
NathanUbuntu <=>
2026-07-09 23:11:18 -0400

    flanger tuned, noisegate is super finnicky but technically tuned

--- testing/src/noise_gate_eff_tester.v
+++ testing/src/noise_gate_eff_tester.v
@@ -31,7 +31,7 @@ module noise_gate_eff_tester();
     threshold = 16'h0000; attack = 0; release_len = 0; makeup_gain = 16'h2000;
     #50;
     rst_n = 1'b1;
-    threshold = 16'h0800;
+    threshold = 16'h1D00;
     attack = 16'd64;
     release_len = 16'd2048;
     makeup_gain = 16'h2000;
