de97087e8e959da6ccb06eaadb2d9c8e6026d89b
NathanUbuntu <=>
2026-07-10 21:37:13 -0400

    paramater fine-tuning and remove data folder

--- testing/scripts/all-tb.sh
+++ testing/scripts/all-tb.sh
@@ -5,17 +5,17 @@ set -e  # stop on first error
 # NAVIGATE TO TB DIRECTORY (ALIAS)
 cd ../../tb/eff
 
-# FUZZ
+# [FAIL] FUZZ -> compiliation issue
 iverilog -o fuzz_tb.vvp fuzz_tb.v && vvp fuzz_tb.vvp
 
-# DELAY
+# [PASS] DELAY
 iverilog -o delay_tb.vvp delay_tb.v && vvp delay_tb.vvp
 
-# FLANGER
+# [PASS] FLANGER
 iverilog -o flanger_tb.vvp flanger_tb.v && vvp flanger_tb.vvp
 
-# HARD CLIP
+# [FAIL] HARD CLIP -> missing header, 3/11 failed
 iverilog -o hard_clip_tb.vvp hard_clip_tb.v && vvp hard_clip_tb.vvp
 
-# NOISE GATE
+# [FAIL] NOISE GATE -> missing header, 9/26 failed
 iverilog -o noise_gate_tb.vvp noise_gate_tb.v && vvp noise_gate_tb.vvp
